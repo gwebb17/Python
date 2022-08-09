871b19163ca758c9132eff43b9d5fb3cd04d4452
gwebb17 <gwebb1723@gmail.com>
2022-08-09 14:08:39 -0700

    became slightly less confused

---
+++ encapsulation_practice.py
@@ -0,0 +1,34 @@
+#create class
+class Coffee:
+    def __init__(self):
+        self.__pounds = 7
+        self._roast = "dark"
+
+    #create privatevariable
+    def printPrvVar(self):
+        print(self.__pounds)
+
+    #create protectedvariable
+    def printProVar(self, pounds):
+        self.__pounds = pounds 
+
+#create object of the class Coffee()
+coffeeObj = Coffee()
+
+#because __pounds is private we need to call the function printPrvVar to access
+#the pounds variable 
+coffeeObj.printPrvVar()
+
+#because the _roast var is protected not private we can directly print it
+#without calling function
+print(coffeeObj._roast)
+
+#can pass in new argument for __pounds by having it as another parameter in
+#the above function printProVar
+coffeeObj.printProVar(10)
+#now __pounds = 10 so the function printPrvVar now displays 10
+coffeeObj.printPrvVar()
+
+
+
+   
