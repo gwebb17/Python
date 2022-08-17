cd099adb958597cf59fd42782be217fe89c0e50f
gwebb17 <gwebb1723@gmail.com>
2022-08-17 13:26:54 -0700

    finished step 15 django

--- PyCharm_Projects/Hello_World/src/mainapp/profiles/views.py
+++ PyCharm_Projects/Hello_World/src/mainapp/profiles/views.py
@@ -4,4 +4,4 @@ from .models import Profile
 # Create your views here.
 def admin_console(request):
     profiles = Profile.objects.all()
-    return render(request, 'profiles/profiles_page.html', {'profiles': 'profiles'})
+    return render(request, 'profiles_page.html', {'profiles': profiles})
